c353ba745f2b623af07477e1f278026b947811f0
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-01 14:20:37 -0500

    Day 1

--- adventofcode/01-2020.py
+++ adventofcode/01-2020.py
@@ -1,8 +1,9 @@
-""" find the two entries that sum to 2020
-and then multiply those two numbers together """
+""" Day 1 """
 
 
-def main():
+def main_a():
+    """ find the two entries that sum to 2020
+    and then multiply those two numbers together """
     input_data = "01-2020.txt"
     data = []
     with open(input_data) as lines:
@@ -15,5 +16,22 @@ def main():
                 return (data[i] * data[j])
 
 
+def main_b():
+    """ find the three entries that sum to 2020
+    and then multiply those three numbers together """
+    input_data = "01-2020.txt"
+    data = []
+    with open(input_data) as lines:
+        line = lines.readline()
+        for line in lines:
+            data.append(int(line.strip()))
+    for i in range(len(data)):
+        for j in range(len(data)):
+            for k in range(len(data)):
+                if (data[i] + data[j] + data[k] == 2020):
+                    return (data[i] * data[j] * data[k])
+
+
 if __name__ == "__main__":
-    print(main())
+    print(main_a())
+    print(main_b())
