6497fcbde0bff0877f0d16b5a336894265a037c2
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-12-06 15:55:06 -0500

    Training

--- 0x02/06-2020.py
+++ 0x02/06-2020.py
@@ -24,5 +24,35 @@ def main_one(filename):
     return sumCounts
 
 
+def main_two(filename):
+    """ Return the sum of the counts to which everyone answered 'yes' """
+    with open(filename, "r") as f:
+        data = f.readlines()
+    answers = []
+    group = []
+    for datum in data:
+        if datum.isspace() is not True:
+            datum = datum.strip()
+            group.append(datum)
+        if datum.isspace() is True:
+            answers.append(group)
+            group = []
+    allYes = 0
+    for group in answers:
+        yes = ""
+        for person in group:
+            yes += person
+        questions = set(yes)
+        for question in questions:
+            answYes = 0
+            for person in group:
+                if person.find(question) is not -1:
+                    answYes += 1
+            if answYes == len(group):
+                allYes += 1
+    return allYes
+
+
 if __name__ == "__main__":
     print(main_one("06-2020.txt"))
+    print(main_two("06-2020.txt"))
