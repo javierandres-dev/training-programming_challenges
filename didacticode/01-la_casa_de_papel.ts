cf56d97b85fc2d2f55baeb4a8367b203af2d542a
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-04-13 17:04:31 -0500

    Training

--- didacticode/01-la_casa_de_papel.ts
+++ didacticode/01-la_casa_de_papel.ts
@@ -19,9 +19,35 @@ const GANG: Array<string> = [
   'Manilla',
   'Oslo',
 ];
+/**
+ * First solution
+ *
+function isMember(member: string): boolean {
+  let i: number = 0;
+  while (i < GANG.length) {
+    if (member === GANG[i]) {
+      return true;
+    }
+    i++;
+  }
+  return false;
+}
+*/
 
+/**
+ * Second solution
+ */
+const isMember = (member: string) => {
+  if (GANG.find((el) => console.log(member === el))) true;
+};
+
+/**
+ * solution
+ *
 const isMember = (member: string): boolean => GANG.includes(member);
+*/
 
+// tests
 console.log(isMember('Bogotá'));
 console.log(isMember('Barcelona'));
 console.log(isMember('Río'));
