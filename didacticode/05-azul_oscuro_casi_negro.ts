f014778872513bbcfe7e21b14cdd1d690bc15f9b
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-04-18 18:30:07 -0500

    Training

--- didacticode/05-azul_oscuro_casi_negro.ts
+++ didacticode/05-azul_oscuro_casi_negro.ts
@@ -1,18 +1,80 @@
 /* Transformar un código en RGB en hexadecimal.
-* Ejemplo de entrada: 0, 36, 255
-* Ejemplo de salida: #0024FF */
-function rgbToHexadecimal(rgb:string): string {
-  let hexCode: string = '#', i: number = 0;
-  const rgbCodes: Array<number> = rgb.split(' ').map(el => parseInt(el));
-  const hexCodes = rgbCodes.map(el => el.toString(16));
-  while (i < hexCodes.length) {
-    parseInt(hexCodes[i]) < 10
-    ? hexCode += `0${hexCodes[i]}`
-    : hexCode += hexCodes[i];
-    i++;
+ * Ejemplo de entrada: 0, 36, 255
+ * Ejemplo de salida: #0024FF */
+const isValid = (aNumber: number): boolean =>
+  aNumber < 0 || aNumber > 255 ? false : true;
+
+/**
+ * First solution
+ */
+function rgbToHexadecimal1(r: number, g: number, b: number): string {
+  if (isValid(r) && isValid(g) && isValid(b)) {
+    let hexCode: string = '#',
+      i: number = 0;
+    const rgbCodes: Array<number> = [r, g, b];
+    const hexCodes = rgbCodes.map((el) => el.toString(16));
+    while (i < hexCodes.length) {
+      if (hexCodes[i].length === 1) {
+        hexCode += `0${hexCodes[i]}`;
+      } else {
+        hexCode += hexCodes[i];
+      }
+      i++;
+    }
+    return hexCode.toUpperCase();
+  } else {
+    return 'Error: Only accept number between 0 to 255.';
   }
-  return hexCode.toUpperCase();
 }
 
+/**
+ * Second solution
+ */
+const rgbToHexadecimal2_ = (aNumber: number): string => {
+  let hexCode: string = '';
+  const charHex = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+  ];
+  let remainder: number = 0;
+  while (aNumber >= 16) {
+    remainder = aNumber % 16;
+    hexCode = `${charHex[remainder]}${hexCode}`;
+    aNumber = Math.floor(aNumber / 16);
+  }
+  hexCode = `${charHex[aNumber]}${hexCode}`;
+  if (hexCode.length === 1) {
+    hexCode = '0' + hexCode;
+  }
+  return hexCode;
+};
+const rgbToHexadecimal2 = (r: number, g: number, b: number): string => {
+  if (isValid(r) && isValid(g) && isValid(b)) {
+    return `#${rgbToHexadecimal2_(r)}${rgbToHexadecimal2_(
+      g
+    )}${rgbToHexadecimal2_(b)}`;
+  } else {
+    return 'Error: Only accept number between 0 to 255.';
+  }
+};
+
 // tests
-console.log(rgbToHexadecimal('0, 36, 255'));
+console.log(rgbToHexadecimal2(0, 36, 255));
+console.log(rgbToHexadecimal2(255, 36, 123));
+console.log(rgbToHexadecimal2(12, 234, 7));
+console.log(rgbToHexadecimal2(0, 0, 0));
+console.log(rgbToHexadecimal2(255, 255, 255));
