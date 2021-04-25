cbc72166eb482a43f42c976c84beb64e82defdc0
1270 <1270@holbertonschool.com>
2021-04-24 20:10:06 -0500

    Training

--- didacticode/06-regreso_al_futuro.ts
+++ didacticode/06-regreso_al_futuro.ts
@@ -5,11 +5,12 @@ Ejemplo de entrada: (MES DÍA AÑO HORAS MINUTOS)
 * Presente: OCT 26 1985 01 21
 * Destino: JUN 01 2020 17 00
 Ejemplo de salida:
-* Vas a viajar al futuro 14 años, 222 días, 15 horas y 39 minutos. */
+* Vas a viajar al futuro 34 años, 227 días, 15 horas y 39 minutos. */
 
 /**
  * First solution
  */
+
 interface Input {
   month: string;
   day: number;
@@ -18,19 +19,58 @@ interface Input {
   minutes: number;
 }
 
-const backToTheFuture1 = (aOrigin: Input, aDestination: Input): string => {
-  //console.log(aOrigin.);
-  //console.log(aDestination);
-  //const origin = new Date('August 19, 1975 23:15:30');
-  const origin = new Date(
+const backToTheFuture = (aOrigin: Input, aDestination: Input): string => {
+  const origin: any = new Date(
     `${aOrigin.month} ${aOrigin.day}, ${aOrigin.year} ${aOrigin.hours}:${aOrigin.minutes}`
   );
-  console.log(origin);
-  return 'working ... ';
+
+  const destination: any = new Date(
+    `${aDestination.month} ${aDestination.day}, ${aDestination.year} ${aDestination.hours}:${aDestination.minutes}`
+  );
+
+  if (origin - destination === 0) return 'No vas a viajar en el tiempo.';
+
+  const difference: number = origin - destination,
+    second: number = 1000,
+    minute: number = second * 60,
+    hour: number = minute * 60,
+    day: number = hour * 24,
+    year: number = day * 365;
+  let toThe: string;
+
+  Math.sign(difference) === -1 ? (toThe = 'futuro') : (toThe = 'pasado');
+
+  const years = Math.floor(Math.abs(difference) / year);
+  let rest: number = Math.abs(difference) - years * year;
+  const days = Math.floor(rest / day);
+  rest -= days * day;
+  const hours = Math.floor(rest / hour);
+  rest -= hours * hour;
+  const minutes = Math.floor(rest / minute);
+
+  return `Vas a viajar al ${toThe} ${years} años, ${days} días, ${hours} horas y ${minutes} minutos.`;
 };
 
 // tests
-backToTheFuture1(
+let res: string;
+res = backToTheFuture(
+  {
+    month: 'APR',
+    day: 24,
+    year: 2021,
+    hours: 19,
+    minutes: 40,
+  },
+  {
+    month: 'APR',
+    day: 24,
+    year: 2021,
+    hours: 19,
+    minutes: 40,
+  }
+);
+console.log(res);
+res = backToTheFuture(
   {
     month: 'OCT',
     day: 26,
@@ -46,3 +86,72 @@ backToTheFuture1(
     minutes: 0,
   }
 );
+console.log(res);
+res = backToTheFuture(
+  {
+    month: 'OCT',
+    day: 26,
+    year: 1985,
+    hours: 1,
+    minutes: 21,
+  },
+  {
+    month: 'JUN',
+    day: 15,
+    year: 2010,
+    hours: 15,
+    minutes: 30,
+  }
+); // future 24y 238d 14h 9m
+console.log(res);
+res = backToTheFuture(
+  {
+    month: 'OCT',
+    day: 26,
+    year: 1985,
+    hours: 1,
+    minutes: 21,
+  },
+  {
+    month: 'JUN',
+    day: 15,
+    year: 1975,
+    hours: 15,
+    minutes: 30,
+  }
+); // past 10y 135d 9h 51m
+console.log(res);
+res = backToTheFuture(
+  {
+    month: 'APR',
+    day: 24,
+    year: 2021,
+    hours: 19,
+    minutes: 39,
+  },
+  {
+    month: 'APR',
+    day: 24,
+    year: 2021,
+    hours: 19,
+    minutes: 40,
+  }
+);
+console.log(res);
+res = backToTheFuture(
+  {
+    month: 'APR',
+    day: 24,
+    year: 2021,
+    hours: 19,
+    minutes: 40,
+  },
+  {
+    month: 'APR',
+    day: 24,
+    year: 2021,
+    hours: 19,
+    minutes: 39,
+  }
+);
+console.log(res);
