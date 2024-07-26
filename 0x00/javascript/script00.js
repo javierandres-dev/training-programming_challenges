26289617c4b7751c7365a9b56356085a81e944af
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-07-26 17:09:28 -0500

    training

---
+++ 0x00/javascript/script00.js
@@ -0,0 +1,36 @@
+'use strict';
+/*
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ * lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios en el
+ * lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos del
+ * lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ */
+
+// ecma international: https://ecma-international.org/
+// oracle javascript: https://www.oracle.com/developer/javascript/
+// mdn javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+/*
+  Comments in
+  Multi-line
+*/
+
+// Comment in single line
+
+var varVariable = "It's a variable";
+let letVariable = "It's a variable";
+const constVariable = "It's a variable type constant";
+
+const aString = String();
+const aNumber = Number();
+const aBoolean = Boolean();
+const aUndefined = undefined;
+const aNull = null;
+const aBigint = BigInt(Number.MAX_SAFE_INTEGER);
+const aSymbol = Symbol();
+
+console.log('¡Hola, JavaScript!');
--- 0x00/readme.md
+++ 0x00/readme.md
@@ -2,6 +2,20 @@
 ## Headings
 Self-training ― Programming, Algorithms, Logic :bulb: :keyboard:
 ### Taken from [MoureDev](https://github.com/mouredev/retos-programacion-2023)
+00
+```
+/*
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ * lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios en el
+ * lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos del
+ * lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ */
+```
+01
 ```
 /*
  * Escribe un programa que muestre por consola (con un print) los
@@ -12,6 +26,7 @@ Self-training ― Programming, Algorithms, Logic :bulb: :keyboard:
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  */
 ```
+02
 ```
 /*
  * Escribe un programa que reciba un texto y transforme lenguaje natural a
