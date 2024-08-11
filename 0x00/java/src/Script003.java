c171291585993a8a6c425619ad15cff0978a1940
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-08-11 17:18:57 -0500

    training

--- 0x00/java/src/Script003.java
+++ 0x00/java/src/Script003.java
@@ -1,33 +1,36 @@
 /*
  * The prime factors of 13195 are 5, 7, 13 and 29.
  * What is the largest prime factor of the number 600851475143?
- * - Checked answer:
+ * - Checked answer: 6857
  */
 public class Script003 {
   public static void main(String[] args) {
-    long largestPrimeFactor = getLargestPrimeFactor(600851475143l);
-    System.out.println(largestPrimeFactor);
-  }
-
-  private static long getLargestPrimeFactor(long num) {
+    long number = 600851475143l;
     long largestPrimeFactor = 0;
-    long i = 2;
-    while (i < num) {
-      if (isPrime(i))
-        if (num % i == 0)
-          largestPrimeFactor = i;
-      i++;
+    long smallestPrimeNumber = 2;
+    long result = number;
+    while (largestPrimeFactor == 0) {
+      boolean isDivisible = result % smallestPrimeNumber == 0;
+      if (isDivisible) {
+        result = result / smallestPrimeNumber;
+        if (result == 1)
+          largestPrimeFactor = smallestPrimeNumber;
+      } else
+        smallestPrimeNumber = getSmallestPrimeNumber(smallestPrimeNumber, number);
     }
-    return largestPrimeFactor;
+    System.out.println("largestPrimeFactor: " + largestPrimeFactor);
   }
 
-  private static boolean isPrime(long num) {
-    long i = 2;
-    while (i < num) {
-      if (num % i == 0)
-        return false;
-      i++;
+  private static long getSmallestPrimeNumber(long from, long to) {
+    from++;
+    for (long num = from; num < to; num++) {
+      boolean isPrime = true;
+      for (long n = 2; n < num; n++)
+        if (num % n == 0)
+          isPrime = false;
+      if (isPrime)
+        return num;
     }
-    return true;
+    return 2;
   }
 }
